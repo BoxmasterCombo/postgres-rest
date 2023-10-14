415d6691508ae450016dfd1778a00f54e2106c2f
hrach <hrachgalstyann@gmail.com>
2023-10-15 00:21:08 +0400

    Add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { setupSwagger } from 'src/swagger/swagger';
 
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: false,
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.setGlobalPrefix('/api');
+
+  setupSwagger(app);
 
   await app.listen(PORT);
 }
 
 bootstrap()
-  .then(() => console.log(`App is running on port ${PORT}`))
+  .then(() =>
+    console.log(`Application is running on: http://localhost:${PORT}`),
+  )
   .catch(console.error);
